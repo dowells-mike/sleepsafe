f4905e00834f7220abfc198f74ad9d756e6e34c6
Dowells Mike <mikedowells400@gmail.com>
2024-11-11 20:31:20 +0000

    Added the Home Screen and nav buttons

--- app/src/main/java/com/example/sleepsafe/MainActivity.kt
+++ app/src/main/java/com/example/sleepsafe/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sleepsafe.ui.theme.SleepsafeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SleepsafeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//Homescreen activity
+fun HomeScreen() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(text = "Hours Slept: ", modifier = Modifier.padding(bottom = 8.dp))
+        Text(text = "Sleep Quality Score: ", modifier = Modifier.padding(bottom = 16.dp))
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SleepsafeTheme {
-        Greeting("Android")
+        Button(onClick = {
+            // Navigating to Analysis Page
+            // todo: write the navigation logic
+        }) {
+            Text(text = "View Analysis")
+        }
+
+        Button(onClick = {
+            // Navigating to settings
+            // todo: write the navigation logic
+        }, modifier = Modifier.padding(top = 8.dp)) {
+            Text(text = "Settings")
+        }
     }
 }
